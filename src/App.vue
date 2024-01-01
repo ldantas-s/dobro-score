e3ed7838d1c25fbd3592a9ad0e9e20b7f0edb522
ldantas-s <lucianostudant@gmail.com>
2024-01-01 17:07:32 -0300

    refactor: update the App file with components

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,35 @@
 <script setup lang="ts">
-import { reactive, ref } from 'vue';
+import { provide, reactive, ref } from 'vue';
 
 import Game from './entity/Game'
 
-import Row from './components/Row.vue'
-import RoundTitle from './components/RoundTitle.vue'
-import Table from './components/Table.vue';
-import TableBodyClimb from './components/TableBodyClimb.vue';
-import Button from './components/Button.vue';
-import InputPlayer from './components/InputPlayer.vue';
+import RoundView from './View/RoundView.vue';
+import ClimbView from './View/ClimbView.vue';
+import WinnerView from './View/WinnerView.vue';
 
 const game = reactive(new Game())
-let isRoundStarted = ref(false)
 
-const toggleRound = () => isRoundStarted.value = !isRoundStarted.value
+type StatusView = 'score' | 'climb' | 'win'
+const statusView = ref<StatusView>('score')
 
-const startRound = () => {
-  game.startRound()
-  toggleRound()
+const toggleView = (status: StatusView) => {
+  statusView.value = status
 }
-const finishRound = () => {
-  toggleRound();
-  game.finishRound()
-}
+const checkStatusView = (status: StatusView) => ({ 
+  isScoreView: status === 'score', 
+  isClimbView: status === 'climb', 
+  isWinView: status === 'win'
+})
+
+provide<InstanceType<typeof Game>>('game', game as Game)
+provide('toggleView', toggleView)
 
 </script>
 
 <template>
-  <section class="round-info" v-show="!isRoundStarted">
-    <RoundTitle data-test="round-title__rounds" :title="`Rodadas: ${game.currentRound()} / 3`" />
-    <h3 class="distribuition-cards">Distribuir {{ game.cardToDistribute() }} cartas</h3>
-    <InputPlayer :game="(game as Game)" />
-    <Table :heads="['Jogador', 'Pontos']">
-      <TableBodyClimb :players="game.playersOrderByPoints" />
-    </Table>
-    <Button data-test="start_round" :disabled="game.hasNotPlayersEnough()" :click="startRound">Começar Rodada {{
-      game.currentRound() + 1 }}</Button>
-  </section>
-  <section class="climbing-info" v-show="isRoundStarted">
-    <h1>Escaladas</h1>
-    <RoundTitle data-test="round-title__climbs" :title="`Rodada atual: ${game.currentRound()}`" />
-    <Table :heads="['Nome', 'Esquecido', 'Q. Cartas', 'Posição (Mais Cartas)', 'Pontos']">
-      <tr v-for="(player, key) in game.playersOrderByCards" :key="player.name">
-        <Row :player="player" :order="key + 1" />
-      </tr>
-    </Table>
-    <Button data-test="finish_round" :click="finishRound">Finalizar Rodada</Button>
-  </section>
-  <section v-show="game.isFinished()">
-    <h1 class="winner-congrats">Congratulations! {{ game.winnerPlayer?.name }} is the Winner with {{
-      game.winnerPlayer?.points }} points</h1>
-  </section>
+  <RoundView v-show="checkStatusView(statusView).isScoreView" />
+  <ClimbView v-show="checkStatusView(statusView).isClimbView" />
+  <WinnerView v-show="checkStatusView(statusView).isWinView" />
 </template>
 
 <style scoped></style>
---
+++ src/View/WinnerView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { inject } from 'vue';
+import Game from '../entity/Game';
+
+const game = inject('game') as Game
+</script>
+
+<template>
+  <section data-test="view-winner">
+    <h1 class="winner-congrats">Congratulations! {{ game.winnerPlayer?.name }} is the Winner with {{
+      game.winnerPlayer?.points }} points</h1>
+  </section>
+</template>
+<style scoped></style>
