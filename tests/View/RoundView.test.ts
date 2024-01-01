eefcdc2bbf8c19a0e429015ef8bdca7fa5cc7ce9
ldantas-s <lucianostudant@gmail.com>
2024-01-01 17:02:16 -0300

    lint: update lint

--- tests/View/RoundView.test.ts
+++ tests/View/RoundView.test.ts
@@ -1,15 +1,15 @@
-import {vi} from 'vitest'
+import { vi } from 'vitest';
 import { mount } from '@vue/test-utils';
-import RoundViewVue from '../../src/View/RoundView.vue';
+import RoundView from '../../src/View/RoundView.vue';
 import Game from '../../src/entity/Game';
 
 test('should show that need to distribute 5 cards if 6 players was registered', async () => {
-  const game = new Game()
-  const toggleView = vi.fn()
-  for (let i=0;i<=6;i++){
-    game.addPlayer(String(i))
+  const game = new Game();
+  const toggleView = vi.fn();
+  for (let i = 0; i <= 6; i++) {
+    game.addPlayer(String(i));
   }
-  const wrapper = mount(RoundViewVue, {
+  const wrapper = mount(RoundView, {
     global: { provide: { game, toggleView } },
   });
   expect(wrapper.find('.distribuition-cards').text()).toBe(
@@ -18,39 +18,42 @@ test('should show that need to distribute 5 cards if 6 players was registered', 
 });
 
 test('should disable the start round button if has not enough player', async () => {
-  const game = new Game()
-  const toggleView = vi.fn()
-  game.addPlayer('A')
-  const wrapper = mount(RoundViewVue, {
+  const game = new Game();
+  const toggleView = vi.fn();
+  game.addPlayer('A');
+  const wrapper = mount(RoundView, {
     global: { provide: { game, toggleView } },
   });
 
-  expect(wrapper.find('[data-test="start_round"]').attributes('disabled')).toBe('')
+  expect(wrapper.find('[data-test="start_round"]').attributes('disabled')).toBe(
+    ''
+  );
 });
 
 test('should enable the start round button if has at least 2 player', async () => {
-  const game = new Game()
-  const toggleView = vi.fn()
-  game.addPlayer('A')
-  game.addPlayer('B')
-  const wrapper = mount(RoundViewVue, {
+  const game = new Game();
+  const toggleView = vi.fn();
+  game.addPlayer('A');
+  game.addPlayer('B');
+  const wrapper = mount(RoundView, {
     global: { provide: { game, toggleView } },
   });
 
-  expect(wrapper.find('[data-test="start_round"]').attributes('disabled')).toBeUndefined()
+  expect(
+    wrapper.find('[data-test="start_round"]').attributes('disabled')
+  ).toBeUndefined();
 });
 
 test('should call the toggleView with climb', async () => {
-  const game = new Game()
-  const toggleView = vi.fn()
-  game.addPlayer('A')
-  game.addPlayer('B')
-  const wrapper = mount(RoundViewVue, {
+  const game = new Game();
+  const toggleView = vi.fn();
+  game.addPlayer('A');
+  game.addPlayer('B');
+  const wrapper = mount(RoundView, {
     global: { provide: { game, toggleView } },
   });
 
-  await wrapper.find('[data-test="start_round"]').trigger('click')
+  await wrapper.find('[data-test="start_round"]').trigger('click');
 
-  expect(toggleView).toHaveBeenCalledWith('climb')
-
+  expect(toggleView).toHaveBeenCalledWith('climb');
 });
