cab6b4718b36e00ffc28dac28b927ed3b5deb3dd
ldantas-s <lucianostudant@gmail.com>
2024-01-09 15:29:47 -0300

    fix: remove unnecessary @change property

--- src/components/Row.vue
+++ src/components/Row.vue
@@ -6,20 +6,14 @@ type Row = {
   order: number
 }
 defineProps<Row>()
-
-const handleChangeCards = (event: Event, player: Player) => {
-  const input = event.target as HTMLInputElement
-  player.cards = Number(input.value)
-}
-
 </script>
 
 <template>
-  <td class="player-climb-name" :data-test="`${player.name}-name`">{{ player.name }}</td>
-  <td><input type="radio" name="forgoteen" class="player-climb-forgotten" /></td>
-  <td>
+  <td class="p-8" :data-test="`${player.name}-name`">{{ player.name }}</td>
+  <td class="p-8"><input type="radio" name="forgoteen" class="player-climb-forgotten" /></td>
+  <td class="p-8">
     <input type="number" :data-test="`${player.name}-cards`" class="player-climb-cards__input"
-      @change="handleChangeCards($event, player)" v-model.lazy="player.cards" />
+      v-model.lazy="player.cards" />
   </td>
   <td class="player-climb-position" :data-test="`${player.name}-position`">{{ order }}o</td>
   <td class="player-climb-point" :data-test="`${player.name}-points`">{{ order }}</td>
