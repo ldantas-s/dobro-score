fd3d0d360e47f74a7223473794401a78088baed3
ldantas-s <lucianostudant@gmail.com>
2024-02-28 17:33:00 -0300

    feat: add button to reset the game

--- src/view/WinnerView.vue
+++ src/view/WinnerView.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 import { inject } from 'vue';
 import Game from '../entity/Game';
-
+import Button from '../components/Button.vue';
 const game = inject('game') as Game
-</script>
+const toggleView = inject('toggleView') as Function
 
+const newGame = () => {
+  toggleView('score')
+  game.reset()
+}
+</script>
 <template>
-  <section data-test="view-winner">
-    <h1 class="winner-congrats">Congratulations! {{ game.winnerPlayer?.name }} is the Winner with {{
-      game.winnerPlayer?.points }} points</h1>
+  <section data-test="view-winner" class="d-flex flex-column">
+    <h1 class="winner-congrats text-center my-16 title-1">
+      Congratulations! {{ game.winnerPlayer?.name }} is the Winner with {{ game.winnerPlayer?.points }} points
+    </h1>
+    <Button @click="newGame()">Novo jogo</Button>
   </section>
 </template>
 <style scoped></style>
