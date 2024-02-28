58b6e4fe12c9b82309f10dab4e444575674c5453
ldantas-s <lucianostudant@gmail.com>
2024-02-28 17:29:18 -0300

    feat: add focus on the add input player

--- src/components/InputPlayer.vue
+++ src/components/InputPlayer.vue
@@ -1,18 +1,30 @@
 <script setup lang="ts">
-import { inject, ref } from 'vue';
+import { inject, onMounted, ref } from 'vue';
 import Button from './Button.vue';
 import Game from '../entity/Game';
 
-const playerName = ref('')
+const playerName = ref<HTMLInputElement | null>(null)
 const game = inject('game') as Game
-const addPlayer = () => {
-	game.addPlayer(playerName.value)
-	playerName.value = ''
+
+const clearRefInputAndFocus = () => {
+	if (!playerName.value) return
+	playerName.value.value = ''
+	playerName.value.focus()
 }
+
+const addPlayer = () => {
+	if (!playerName.value) return
+	game.addPlayer(playerName.value.value)
+	clearRefInputAndFocus()
+
+}
+
+onMounted(clearRefInputAndFocus)
 </script>
 <template>
 	<div v-show="game.isIdle()" class="d-flex flex-column gap-8">
-		<input type="text" data-test="input-player" class="rounded-2 text-black p-16 text-center bg-white bold-400" v-model.trim="playerName" />
+		<input type="text" data-test="input-player" class="rounded-2 text-black p-16 text-center bg-white bold-400"
+			ref="playerName" />
 		<Button data-test="add-player__button" @click="addPlayer" :disabled="game.hasLimitPlayers()">
 			Adicionar Jogador
 		</Button>
